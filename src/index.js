fac1e675be95e3942d4143c1fc806c21ce3082f4
Guilherme <guilhermesevero100@gmail.com>
2026-01-07 14:15:21 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { useState, useEffect, createContext, useContext } from 'react';
 import ReactDOM from 'react-dom/client';
-import style from './Styles/style1.css';
+import style from './Styles/style2.css';
 import { isFocusable } from '@testing-library/user-event/dist/utils/index.js';
-import JogoDaVelha from './JogoDaVelha.js';
+import Receitas from './Receitas.js';
 
 
 
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <>
-      <JogoDaVelha />
+      <Receitas />
     </>
   );
 }
